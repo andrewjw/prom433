fb5bd5d8cb401a1f0743d5f76f216320edeeeaac
Andrew Wilkinson <andrewjwilkinson@gmail.com>
2021-09-30 19:22:55 +0000

    Add debug to buildbot script.

--- buildbot.sh
+++ buildbot.sh
@@ -10,7 +10,9 @@ pip3 install -r requirements.txt
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 echo "Building branch $BRANCH"
 if [[ "$BRANCH" == "master" ]]; then
-  COVERALLS_REPO_TOKEN=$GLOWPROM_COVERALLS_REPO_TOKEN coveralls
+  echo coverage
+  COVERALLS_REPO_TOKEN=$PROM433_COVERALLS_REPO_TOKEN coveralls
+  echo $?
   semantic-release publish
 fi
 if [[ ${BRANCH:0:7} == "heads/v" ]]; then
