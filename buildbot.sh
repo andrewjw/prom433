7b30d6fa9ba4cff606cd82bce06e9f08603c789a
Andrew Wilkinson <andrewjwilkinson@gmail.com>
2023-03-03 21:22:41 +0000

    chore: Test on multiple Python versions.

--- buildbot.sh
+++ buildbot.sh
@@ -2,10 +2,23 @@
 
 set -e
 
-pip3 install -r requirements.txt
+pip3.8 install -r requirements.txt
+
+COVERAGE=coverage-3.8 ./run_tests.sh
+
+pip3.9 install -r requirements.txt
+
+COVERAGE=coverage-3.9 ./run_tests.sh
+
+pip3.10 install -r requirements.txt
+
+COVERAGE=coverage-3.10 ./run_tests.sh
+
+pip3.11 install -r requirements.txt
+
+COVERAGE=coverage-3.11 ./run_tests.sh
 
 ./code_style.sh
-./run_tests.sh
 
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 echo "Building branch $BRANCH"
