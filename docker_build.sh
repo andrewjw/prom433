8b8387ce0a4a2333f1ea3fd0c5145916f739f7a2
Andrew Wilkinson <andrewjwilkinson@gmail.com>
2024-07-12 17:08:09 +0100

    fix: Fix multi arch docker build.

--- docker_build.sh
+++ docker_build.sh
@@ -6,6 +6,8 @@ if [ $# -eq 1 ]; then
   sudo docker login --username andrewjw --password $DOCKER_TOKEN
 fi
 
+sudo docker buildx create --use
+
 sudo docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 \
                          --build-arg VERSION=$TAG \
                          -t andrewjw/prom433:$TAG \
